3f96702429187c35a0aaa209b425555289d3f94e
max005 <a29325139@gmail.com>
2015-02-05 13:45:52 +0800

    Initial Commit

---
+++ OpenCV_CL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV_CL
+//
+//  Created by 黃昱彰 on 2015/2/5.
+//  Copyright (c) 2015年 黃昱彰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
